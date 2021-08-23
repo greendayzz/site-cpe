648e6df8ebf21b1afee3fea8c99e1db9d48913e7
lelliam <1113408083@qq.com>
2021-08-23 14:48:08 +0800

    init

--- site-cpe/src/App.vue
+++ site-cpe/src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-   
+    <app-map></app-map>
   </div>
 </template>
-
+<script>
+import AppMap from "./components/AppMap.vue";
+export default {
+  components: {
+    AppMap,
+  },
+};
+</script>
 <style>
-
+html,body{
+  margin: 0;
+  padding: 0;
+}
+#app{
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
 </style>
